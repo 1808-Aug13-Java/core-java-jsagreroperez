62fe9056728031eaff6970dcb4b1cf43202a237b
jsagreroperez <jsagreroperez@gmail.com>
2018-08-15 09:45:28 -0400

    Removed folder form repository

--- FourPillars/src/com/revature/animals/FlightlessBirds.java
+++
@@ -1,5 +0,0 @@
-package com.revature.animals;
-//subclass of Birds which means it inherits the members of Animals
-public class FlightlessBirds extends Birds {
-	static final boolean FLIGHT = false;
-}
--- FourPillars/src/com/revature/animals/SoaringBirds.java
+++
@@ -1,5 +0,0 @@
-package com.revature.animals;
-// subclass of Birds which means it inherits the members of Animals
-public class SoaringBirds {
-
-}
